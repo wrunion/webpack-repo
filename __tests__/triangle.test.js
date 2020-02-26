6264a3686b94fa4dbd170bcbbfdad9e1e8c8c1bf
Winter Runion <winterrunion@gmail.com>
2020-02-26 00:04:52 -0800

    Write test to see if input is an array.

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -30,8 +30,6 @@ describe('Triangle', () => {
     expect(equilateralTriangle.checkType()).toEqual('equilateral triangle');
   });
 
-<<<<<<< Updated upstream
-=======
   test('should not accept words as arguments', () => {
     let words = new Triangle("la", "de", "da");
     expect(words.checkType()).toEqual('error');
@@ -41,5 +39,4 @@ describe('Triangle', () => {
     let array = new Triangle([7],[4],[5]);
     expect(array.checkType()).toEqual('error');
   });
->>>>>>> Stashed changes
 });
--- src/triangle.js
+++ src/triangle.js
@@ -8,7 +8,10 @@ Triangle.prototype.checkType = function() {
   let notNum = /\D/g;
   if (notNum.test(this.side1) || notNum.test(this.side2) || notNum.test(this.side3)) {
     return "error";
-  } else if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+  } 
+  if ((typeof this.side1 != "number") || (typeof this.side2 != "number") || (typeof this.side3 != "number")) { return "error"; }
+
+  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
     return "not a triangle";
   } else if ((this.side1 === this.side2) && (this.side2 === this.side3)) {
     return "equilateral triangle";
