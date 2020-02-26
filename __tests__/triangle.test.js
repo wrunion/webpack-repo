9986346601e20d05b018016f23a987142ee27480
Winter Runion <winterrunion@gmail.com>
2020-02-25 20:19:54 -0800

    Add test to check for equilateral triangle.

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -15,5 +15,19 @@ describe('Triangle', () => {
     expect(notTriangle.checkType()).toEqual('not a triangle');
   });
 
+  test('should correctly determine whether three lengths make a scalene triangle', () => {
+    let scaleneTriangle = new Triangle(4,5,7);
+    expect(scaleneTriangle.checkType()).toEqual('scalene triangle');
+  });
+
+  test('should correctly determine whether three lengths make an isoceles triangle', () => {
+    let isoscelesTriangle = new Triangle(5,5,7);
+    expect(isoscelesTriangle.checkType()).toEqual('isosceles triangle');
+  });
+
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+    let equilateralTriangle = new Triangle(3,3,3);
+    expect(equilateralTriangle.checkType()).toEqual('equilateral triangle');
+  });
 
 });
--- src/triangle.js
+++ src/triangle.js
@@ -5,5 +5,11 @@ export function Triangle(side1, side2, side3) {
 }
 
 Triangle.prototype.checkType = function() {
-  return "not a triangle";
+  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+    return "not a triangle";
+  } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+    return "scalene triangle";
+  }  else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+    return "isosceles triangle";
+  }
 };
